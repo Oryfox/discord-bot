f1018f0d4e9c6aea16a0f18106c1401636570343
Semih Kaiser <git@oryfox.de>
2026-05-24 00:10:07 +0200

    Init

---
+++ src/test/java/de/oryfox/discordbot/DiscordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package de.oryfox.discordbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
