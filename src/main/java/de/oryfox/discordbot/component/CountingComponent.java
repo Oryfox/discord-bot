5ed1ee091e0ddb3d1d9931c9d4bbc656ff06a12f
Semih Kaiser <git@oryfox.de>
2026-05-24 01:47:23 +0200

    feat: Added JDA with first feature: Counting

---
+++ src/main/java/de/oryfox/discordbot/component/CountingComponent.java
@@ -0,0 +1,82 @@
+package de.oryfox.discordbot.component;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class CountingComponent extends ListenerAdapter {
+
+    @Value("${channels.counting:}")
+    private String channelId;
+
+    private final JDA jda;
+
+    private Map<Long, Instant> userMap;
+    private long lastNumber = 1;
+
+    @PostConstruct
+    public void initCounting() {
+        if (channelId == null || channelId.isBlank()) {
+            log.error("No counting channel provided. Feature is disabled.");
+            return;
+        }
+
+        userMap = new HashMap<>();
+
+        var channel = jda.getTextChannelById(channelId);
+        if (channel == null) {
+            log.error("Channel with provided id {} not found", channelId);
+        }
+
+        var embed = new EmbedBuilder()
+                .setTitle("Counting")
+                .setDescription("Here you can count together as a community. The rules are simple. No back to back counting. Only one message per member every 12 hours. If someone makes a mistake, everything gets resetted.")
+                .build();
+        channel.sendMessageEmbeds(embed).queue();
+        channel.sendMessage("I'll go first: 1").queue();
+
+        jda.addEventListener(this);
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getChannel().getId().equalsIgnoreCase(channelId)) {
+            var messageContent = event.getMessage().getContentRaw();
+            var user = event.getAuthor();
+
+            if (userMap.containsKey(user.getIdLong()) && Instant.now().isBefore(userMap.get(user.getIdLong()).plus(12, ChronoUnit.HOURS))) {
+                event.getMessage().delete().queue();
+                return;
+            }
+
+            if (messageContent.matches("\\d+")) {
+                var number = Long.parseLong(messageContent);
+                if (number == lastNumber + 1) {
+                    lastNumber = number;
+                    userMap.put(user.getIdLong(), Instant.now());
+                    event.getMessage().addReaction(Emoji.fromUnicode("U+2705")).queue();
+                } else {
+                    event.getMessage().addReaction(Emoji.fromUnicode("U+274C")).queue();
+                    userMap = new HashMap<>();
+                    lastNumber = 1;
+                    event.getChannel().sendMessage(String.format("<@%s> killed the streak at %d.%n%nLet's start a new try: 1", user.getId(), lastNumber)).queue();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/de/oryfox/discordbot/config/JdaConfig.java
@@ -0,0 +1,32 @@
+package de.oryfox.discordbot.config;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.EnumSet;
+
+@Configuration
+@Slf4j
+public class JdaConfig {
+
+    @Value("${discord.token:}")
+    private String discordToken;
+
+    @Bean(destroyMethod = "awaitShutdown")
+    @SneakyThrows
+    public JDA jda() {
+        if (discordToken == null || discordToken.isBlank()) {
+            log.error("Please provide a Discord Bot token.");
+        }
+        return JDABuilder
+                .create(discordToken, EnumSet.allOf(GatewayIntent.class))
+                .build()
+                .awaitReady();
+    }
+}
