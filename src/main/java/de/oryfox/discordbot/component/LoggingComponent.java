5a5d63072821fb5f0cd2458413b7bf0af72a28da
Semih Kaiser <git@oryfox.de>
2026-05-25 02:41:39 +0200

    feat: Replaced embeds in logging with v2 components to display a timezone independent timestamp

--- src/main/java/de/oryfox/discordbot/component/LoggingComponent.java
+++ src/main/java/de/oryfox/discordbot/component/LoggingComponent.java
@@ -1,19 +1,40 @@
 package de.oryfox.discordbot.component;
 
+import jakarta.annotation.Nonnull;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resources;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.components.actionrow.ActionRow;
+import net.dv8tion.jda.api.components.buttons.Button;
+import net.dv8tion.jda.api.components.container.Container;
+import net.dv8tion.jda.api.components.filedisplay.FileDisplay;
+import net.dv8tion.jda.api.components.mediagallery.MediaGallery;
+import net.dv8tion.jda.api.components.mediagallery.MediaGalleryItem;
+import net.dv8tion.jda.api.components.section.Section;
+import net.dv8tion.jda.api.components.selections.StringSelectMenu;
+import net.dv8tion.jda.api.components.separator.Separator;
+import net.dv8tion.jda.api.components.textdisplay.TextDisplay;
+import net.dv8tion.jda.api.components.thumbnail.Thumbnail;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageBulkDeleteEvent;
 import net.dv8tion.jda.api.events.message.MessageDeleteEvent;
 import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.utils.FileUpload;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.awt.*;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZonedDateTime;
 
 @Component
 @RequiredArgsConstructor
@@ -41,35 +62,67 @@ public class LoggingComponent extends ListenerAdapter {
 
     @Override
     public void onMessageUpdate(MessageUpdateEvent event) {
-        var embed = new EmbedBuilder()
-                .setTitle("Message Updated")
-                .setDescription(String.format("<@%s> updated the message %s in channel %s", event.getAuthor().getId(), event.getMessage().getJumpUrl(), event.getChannel().getJumpUrl()))
-                .setColor(Color.ORANGE)
-                .build();
+        if (event.getAuthor().isBot()) {
+            return;
+        }
 
-        channel.sendMessageEmbeds(embed).queue();
+        var container = Container.of(
+                TextDisplay.of("### Message Updated"),
+
+                Separator.createInvisible(Separator.Spacing.SMALL),
+
+                TextDisplay.of(String.format("**User:** <@%s>", event.getAuthor().getId())),
+                TextDisplay.of(String.format("**Channel:** %s", event.getChannel().getJumpUrl())),
+                TextDisplay.of(String.format("**Link to message:** %s", event.getMessage().getJumpUrl())),
+
+                Separator.createInvisible(Separator.Spacing.SMALL),
+
+                TextDisplay.of(String.format("-# <t:%s:f>", Instant.now().getEpochSecond()))
+        );
+
+        channel.sendMessageComponents(container)
+                .useComponentsV2()
+                .queue();
     }
 
     @Override
     public void onMessageDelete(MessageDeleteEvent event) {
-        var embed = new EmbedBuilder()
-                .setTitle("Message Deleted")
-                .setDescription(String.format("Message with id `%d` deleted in %s", event.getMessageIdLong(), event.getChannel().getJumpUrl()))
-                .setColor(Color.RED)
-                .build();
+        var container = Container.of(
+                TextDisplay.of("### Message Deleted"),
 
-        channel.sendMessageEmbeds(embed).queue();
+                Separator.createInvisible(Separator.Spacing.SMALL),
+
+                TextDisplay.of(String.format("**Channel:** %s", event.getChannel().getJumpUrl())),
+                TextDisplay.of(String.format("**Message-ID:** `%s`", event.getMessageId())),
+
+                Separator.createInvisible(Separator.Spacing.SMALL),
+
+                TextDisplay.of(String.format("-# <t:%s:f>", Instant.now().getEpochSecond()))
+        );
+
+        channel.sendMessageComponents(container)
+                .useComponentsV2()
+                .queue();
     }
 
     @Override
     public void onMessageBulkDelete(MessageBulkDeleteEvent event) {
-        var embed = new EmbedBuilder()
-                .setTitle("Message Deleted")
-                .setDescription(String.format("Messages with ids `%s` deleted in %s", String.join(", ", event.getMessageIds()), event.getChannel().getJumpUrl()))
-                .setColor(Color.RED)
-                .build();
+        var container = Container.of(
+                TextDisplay.of("### Message Deleted"),
 
-        channel.sendMessageEmbeds(embed).queue();
+                Separator.createInvisible(Separator.Spacing.SMALL),
+
+                TextDisplay.of(String.format("**Channel:** %s", event.getChannel().getJumpUrl())),
+                TextDisplay.of(String.format("**Message-IDs:** `%s`", String.join(", ", event.getMessageIds()))),
+
+                Separator.createInvisible(Separator.Spacing.SMALL),
+
+                TextDisplay.of(String.format("-# <t:%s:f>", Instant.now().getEpochSecond()))
+        );
+
+        channel.sendMessageComponents(container)
+                .useComponentsV2()
+                .queue();
     }
 }
 
